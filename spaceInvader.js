e73d0edabaf27f02dfced9385b65b270841e795a
Nolan <nolan.weemaels@student.ehb.be>
2024-10-03 16:09:10 +0200

    Created spaceinvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,19 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+drawSpaceInvader()
+
+function drawSpaceInvader(){
+    context.fillRect(50,50,300,300)
+
+    context.fillStyle = "#B76184"
+    context.fillRect(75,75,50,50);
+    context.fillRect(125,125,50,50);
+    context.fillRect(175,75,50,50);
+    context.fillRect(225,125,50,50);
+    context.fillRect(275,75,50,50);
+    context.fillRect(75,225,50,50);
+    context.fillRect(275,225,50,50);
+}
