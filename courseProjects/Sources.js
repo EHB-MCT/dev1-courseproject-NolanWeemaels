d906d665829f83e35bc9d8c2e1d4af1f935fbab1
Nolan <nolan.weemaels@student.ehb.be>
2024-12-18 23:46:26 +0100

    TAAK KLAAR Nolan Weemaels

---
+++ courseProjects/Sources.js
@@ -0,0 +1,10 @@
+//Source 1
+//to learn how to add a button
+//Team, A. (2023, 9 juni). How to create a button in JavaScript. Altcademy Blog. Geraadpleegd op 18 december 2024, van https://www.altcademy.com/blog/how-to-create-a-button-in-javascript/
+
+
+
+//Source 2
+//To learn how to add a timer
+//Delac, M. (z.d.). [JavaScript] - How to create a countdown timer in JavaScript. Geraadpleegd op 18 december 2024, van https://www.shecodes.io/athena/52336-how-to-create-a-countdown-timer-in-javascript
+
